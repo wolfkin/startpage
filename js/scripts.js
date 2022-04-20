c7773f4c84cc3cfa33c59534b8d4e06a8c8375ce
wolfkin-gungrave <wolfkin@gmail.com>
2022-04-20 15:42:02 -0400

    Some more giant patchings
    added validator badge. cleaned up some HTML things.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-new Date().toDateString() = "Sun Sep 13 2015"
+///////////////////////////////////////////////////////////
+// Clock code from StackOverflow
+//  https://stackoverflow.com/questions/28415178/
+//
+///////////////////////////////////////////////////////////
+
+
+(function(){
+	// reference to clock element
+	var clockElement = document.getElementById( "clock" );
+
+	// a function that will update the contents with the time:
+	function updateClock ( clock ) {
+	    clock.innerHTML = new Date().toLocaleTimeString();
+	}
+
+	// calling this every second to keep the clock up to date
+	setInterval(function () {
+	    updateClock( clockElement );
+	}, 1000);
+
+}());
+
+
+new Date().toDateString() = "Sun Sep 13 2015";
